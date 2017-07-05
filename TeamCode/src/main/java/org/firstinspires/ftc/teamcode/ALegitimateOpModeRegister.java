9564a6566e9df36f29193716606b37d409a1e773
kawaiiPlat <thecaptainglaceon@gmail.com>
2017-07-05 14:39:45 -0400

    Added base classes from old repository.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ALegitimateOpModeRegister.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
+
+/**
+ * Created by kawaiiPlat on 6/10/2017.
+ */
+
+public class ALegitimateOpModeRegister {
+
+
+    public void register(OpModeManager manager) {
+        // Register all the OpModes
+        manager.register("Test Mode", ALegitimateClass.class);
+    }
+
+}
